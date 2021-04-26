2a51e7fb3426fedf2f3577c815238787326cf657
Blanco, Ramiro <rablanco@netsuite.com>
2021-04-26 08:04:06 +0200

    Adds greeting component

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Greeting from './Greeting'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Greeting />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is so cool.
         </p>
         <a
           className="App-link"
