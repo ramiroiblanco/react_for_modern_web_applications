8281e47ce8c91a2c413560be6d19b651ae6ed9e4
Blanco, Ramiro <rablanco@netsuite.com>
2021-05-13 08:27:20 +0200

    Lesson 7: Use Context in React

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -13,48 +13,50 @@ import {
 } from './pages'
 import { NavBar } from './NavBar'
 import { FormsNavBar } from './FormsNavBar'
+import { ThemeContext } from './ThemeContext'
 
 function App () {
-
   return (
-    <div className="App">
-      <Router>
-        <NavBar/>
-        <div className="App-header">
-          <Switch>
-            <Route path="/" exact>
-              <HomePage/>
-            </Route>
-            <Route path="/counter/">
-              <CounterButtonPage/>
-            </Route>
-            <Route path="/people-list">
-              <PeopleListPage/>
-            </Route>
-            <Route path="/protected">
-              <ProtectedPage/>
-            </Route>
-            <Route path="/user">
-              <UserProfilePage/>
-            </Route>
-            <Route path="/forms">
-              <Router>
-                <FormsNavBar/>
-                <Route path="/forms/controlled">
-                  <ControlledFormPage/>
-                </Route>
-                <Route path="/forms/uncontrolled">
-                  <UncontrolledFormPage/>
-                </Route>
-              </Router>
-            </Route>
-            <Route>
-              <NotFoundPage/>
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    </div>
+    <ThemeContext.Provider value='dark'>
+      <div className="App">
+        <Router>
+          <NavBar/>
+          <div className="App-header">
+            <Switch>
+              <Route path="/" exact>
+                <HomePage/>
+              </Route>
+              <Route path="/counter/">
+                <CounterButtonPage/>
+              </Route>
+              <Route path="/people-list">
+                <PeopleListPage/>
+              </Route>
+              <Route path="/protected">
+                <ProtectedPage/>
+              </Route>
+              <Route path="/user">
+                <UserProfilePage/>
+              </Route>
+              <Route path="/forms">
+                <Router>
+                  <FormsNavBar/>
+                  <Route path="/forms/controlled">
+                    <ControlledFormPage/>
+                  </Route>
+                  <Route path="/forms/uncontrolled">
+                    <UncontrolledFormPage/>
+                  </Route>
+                </Router>
+              </Route>
+              <Route>
+                <NotFoundPage/>
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    </ThemeContext.Provider>
   )
 }
 
